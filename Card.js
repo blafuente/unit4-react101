4a3af20203c64b4c5782fb655b3dc800b35d707c
Brian Lafuente <loulafuente@BrianLafuentes-Macbook-Pro.local>
2019-02-12 14:56:52 -0500

    react for the day, feb 12

--- Card.js
+++ Card.js
@@ -13,11 +13,11 @@ function Card(props){
             <div className="col s2">
                 <div className="card hoverable small">
                     <div className="card-image">
-                        <img src={image} />
+                        <img src={props.data.image} /> 
                     </div>
                     <div className="card-content">
-                        <p>{title}</p>
-                        <p>{name}</p>
+                        <p>{props.data.course}</p>
+                        <p>{props.data.instructor}</p>
                     </div>
                     <div className="card-action">
                         <a href="#">${saleOn  ? 9.99 : 59.99}</a>
