1d6361003f44a881690f70eed7e43b4df50f7e8d
Brian Lafuente <loulafuente@BrianLafuentes-Macbook-Pro.local>
2019-02-12 11:48:32 -0500

    added server and multiple cards

---
+++ Card.js
@@ -0,0 +1,28 @@
+// EVERY REACT COMPONENT MUST RETURN A SINGLE DOM ELEMENT
+
+function Card(){
+    const name = "Brian Lafuente";
+    const title = "React From the Beginning";
+    const saleOn = true;
+
+    return (
+        <div className="row">
+            <div className="col s2">
+                <div className="card hoverable small">
+                    <div className="card-image">
+                        <img src="http://lorempixel.com/400/400/technics/" />
+                    </div>
+                    <div className="card-content">
+                        <p>{title}</p>
+                        <p>{name}</p>
+                    </div>
+                    <div className="card-action">
+                        <a href="#">${saleOn  ? 9.99 : 59.99}</a>
+                    </div>
+                </div>
+            </div>    
+        </div>
+    )   
+}
+
+
