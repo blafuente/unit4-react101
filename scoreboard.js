9912c562c8438cb3914ed530951bb04479b52c50
Brian Lafuente <loulafuente@BrianLafuentes-Macbook-Pro.local>
2019-02-13 10:26:30 -0500

    added props

--- scoreboard.js
+++ scoreboard.js
@@ -1,43 +1,42 @@
 function Header(props){
     return(
         <div className="header">
-            <h1>Ping Pong Championship</h1>
+            <h1>{props.title}</h1>
+        </div>
+    )
+}
+
+function Counter(props){
+    return(
+        <div className="counter">
+            <button className="minus waves-effect waves-light btn">-</button>
+            <div className="player-score">0</div>
+            <button className="plus waves-effect waves-light btn">+</button>
+        </div>
+    )
+}
+
+function Player(props){
+    return(
+        <div className="player">
+            <div className="player-name">{props.name}</div>
+            <Counter />
         </div>
     )
 }
 
 // Application is UpperCase... which means it's a Component!
 // All components, without exception, must return a single DOM Element.
-function Application(){
+function Application(props){
     return(
         <div className="containter">
             <div className="row">
-                <div className="col s6 s3-offset board">
-                    
-                    <Header />
-
+                <div className="col s6 s3-offset board">                    
+                    <Header title={props.title}/>
                     <div className="players">
-
-                        <div className="player">
-                            <div className="player-name">Brian</div>
-                            <div className="counter">
-                                <button className="minus waves-effect waves-light btn">-</button>
-                                <div className="player-score">0</div>
-                                <button className="plus waves-effect waves-light btn">+</button>
-                            </div>
-                        </div>
-                        
-                        <div className="player">
-                            <div className="player-name">Brandon</div>
-                            <div className="counter">
-                                <button className="minus waves-effect waves-light btn">-</button>
-                                <div className="player-score">0</div>
-                                <button className="plus waves-effect waves-light btn">+</button>
-                            </div>
-                        </div>
-
+                        <Player name="Brian"/>
+                        <Player name="Player 2"/>
                     </div>
-                
                 </div>
             </div>
         </div>
@@ -50,6 +49,6 @@ function Application(){
 // 1. What
 // 2. Where
 ReactDOM.render(
-    <Application />,
+    <Application title="Ping Pong Tourney"/>,
     document.getElementById('root')
 );
